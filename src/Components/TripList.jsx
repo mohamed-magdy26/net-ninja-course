fc7c9f4ab149e0fb9799230b0129555b2ef5e5ca
mohamed magdy <mohamed.magdy0598@gmail.com>
2023-05-25 04:21:18 +0300

    Starting custom hook

--- src/Components/TripList.jsx
+++ src/Components/TripList.jsx
@@ -1,33 +1,22 @@
-import { useCallback, useEffect, useState } from 'react';
-
+import { useState } from 'react';
+import useFetch from '../hooks/useFetch';
 import './TripList.css';
 
 function TripList() {
-  const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState('http://localhost:8000/trips');
-
-  const fetchTrips = useCallback(async () => {
-    const res = await fetch(url);
-    const data = await res.json();
-    setTrips(data);
-  }, [url]);
-
-  console.log(trips);
-
-  useEffect(() => {
-    fetchTrips();
-  }, [fetchTrips]);
+  const { data: trips } = useFetch(url);
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
       <ul>
-        {trips.map((trip) => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {trips &&
+          trips.map((trip) => (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          ))}
       </ul>
       <div className="filters">
         <button
@@ -48,3 +37,15 @@ function TripList() {
 }
 
 export default TripList;
+
+// const fetchTrips = useCallback(async () => {
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   setTrips(data);
+// }, [url]);
+
+// console.log(trips);
+
+// useEffect(() => {
+//   fetchTrips();
+// }, [fetchTrips]);
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data };
+};
+
+export default useFetch;
